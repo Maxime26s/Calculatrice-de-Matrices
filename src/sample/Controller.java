eb77792869a34b599eb8c4b713c2792ba4d63f52
Maxime <wixxwistiii@gmail.com>
2018-11-09 17:54:55 -0500

    More Progress

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -9,7 +9,6 @@ import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
